286dc015102056ba671b1dbeab2db7af1b59751f
AarushG <me@aarushg.com>
2020-06-24 21:51:09 -0400

    Revert "Update models.py"
    This reverts commit 5bb3707a3b73712216b84ee6f0f1a17b008446ce.

--- app/models.py
+++ app/models.py
@@ -7,9 +7,6 @@ from sqlalchemy.dialects.postgresql import JSON
 
 
 class User(UserMixin, db.Model):
-
-    __tablename__ = 'user'
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
@@ -20,10 +17,6 @@ class User(UserMixin, db.Model):
 
 
 class Data(db.Model):
-
-    __tablename__ = 'data'
-
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15))
     title = db.Column(db.String(100))
